dacd2f395b9a4097ea9fffed6237ee3f522a93ec
Arslan Akhmedov <arslan-akh@yandex.ru>
2022-03-11 10:49:17 +0500

    feat: add base test file

---
+++ src/app/app.test.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom'
+import App from './index'
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+})
